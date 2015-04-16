5117534c4183af8e3d68247d4d47d8082753ea86
Chupligin Sergey <neochapay@gmail.com>
2015-04-16 17:00:03 +0300

    Fix module deprecated message

--- modules/mod_ajaxchat_minichat/module.php
+++ modules/mod_ajaxchat_minichat/module.php
@@ -3,7 +3,7 @@ function mod_ajaxchat_minichat($module_id)
 {
   $inCore = cmsCore::getInstance();
   
-  $smarty = $inCore->initSmarty('modules', 'mod_ajaxchat_minichat.tpl');
+  $smarty = cmsPage::initTemplate('modules', 'mod_ajaxchat_minichat.tpl');
   $smarty->display('mod_ajaxchat_minichat.tpl');
   return true;
 }
